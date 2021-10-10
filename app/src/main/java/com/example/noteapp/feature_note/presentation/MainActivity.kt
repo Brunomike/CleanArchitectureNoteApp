274b6ee7eafd62917211daaa666fd214fb23e8a0
Brunomike <brunomike254@gmail.com>
2021-10-10 20:04:50 +0300

    Finished functionality of the note app clean architecture

--- app/src/main/java/com/example/noteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/MainActivity.kt
@@ -3,24 +3,65 @@ package com.example.noteapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.noteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.example.noteapp.feature_note.presentation.notes.NotesScreen
+import com.example.noteapp.feature_note.presentation.utils.Screen
 import com.example.noteapp.ui.theme.NoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-
+                Surface(color = MaterialTheme.colors.background,modifier = Modifier.fillMaxSize()) {
+                    val navController= rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination =Screen.NotesScreen.route
+                    ){
+                        composable(route=Screen.NotesScreen.route){
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route=Screen.AddEditNoteScreen.route+
+                                    "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(name="noteId"){
+                                    type= NavType.IntType
+                                    defaultValue=-1
+                                },
+                                navArgument(name="noteColor"){
+                                    type= NavType.IntType
+                                    defaultValue=-1
+                                }
+                            )
+                        ){
+                            val color=it.arguments?.getInt("noteColor")?:-1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color)
+                        }
+                    }
                 }
             }
         }
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//    }
 }
 
--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -6,7 +6,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.noteapp.feature_note.domain.model.Note
 import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.example.noteapp.feature_note.domain.utils.NoteOrder
+import com.example.noteapp.feature_note.domain.utils.OrderType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,6 +23,10 @@ class NotesViewModel @Inject constructor(
     val state: State<NotesState> = _state
 
     private var recentlyDeletedNote: Note? = null
+    private var getNotesJob: Job?=null
+    init {
+        getNotes(NoteOrder.Date(OrderType.Descending))
+    }
 
     fun onEvent(event: NotesEvent) {
         when (event) {
@@ -27,6 +36,7 @@ class NotesViewModel @Inject constructor(
                 ){
                   return
                 }
+                getNotes(event.noteOrder)
             }
             is NotesEvent.DeleteNote -> {
                 viewModelScope.launch {
@@ -46,5 +56,16 @@ class NotesViewModel @Inject constructor(
                 )
             }
         }
+    }
+
+    private fun getNotes(noteOrder: NoteOrder){
+        getNotesJob?.cancel()
+        getNotesJob=noteUseCases.getNotes(noteOrder)
+            .onEach { notes->
+                _state.value=state.value.copy(
+                    notes = notes,
+                    noteOrder = noteOrder
+                )
+            }.launchIn(viewModelScope)
     }
 }
